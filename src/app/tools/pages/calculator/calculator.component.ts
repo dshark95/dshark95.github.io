680209eef1bc26cabd7061ebed625062187eeeab
leetatyi@hotmail.com <leetatyi@hotmail.com>
2020-12-21 13:30:34 +0800

    Firts Commit

--- src/app/tools/pages/calculator/calculator.component.ts
+++ src/app/tools/pages/calculator/calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  constructor() { }
+  minValue:number = 0;
+
+  maxPercent:number = 100;
+
+  numberForm!:FormGroup;
+
+  formLabelSpan:number = 7;
+  formFieldSpan:number = 12;
+
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.formInit();
+  }
+
+  formInit():void{
+    this.numberForm = this.fb.group({
+      totalAmount:[0],
+      percentage:[0]
+    });
+  }
+
+  formatterPercent = (value: number) => `${value} %`;
+  parserPercent = (value: string) => value.replace(' %', '');
+
+  calc():number{
+    let multiplier:number = this.numberForm.controls["percentage"].value/100;
+    return this.numberForm.controls["totalAmount"].value * multiplier;
   }
 
 }
--- src/app/tools/tools.module.ts
+++ src/app/tools/tools.module.ts
@@ -4,14 +4,20 @@ import { CommonModule } from '@angular/common';
 import { ToolsRoutingModule } from './tools-routing.module';
 import { CalculatorComponent } from './pages/calculator/calculator.component';
 import { NzInputModule } from 'ng-zorro-antd/input';
-
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import { NzFormModule } from'ng-zorro-antd/form';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CalculatorComponent],
   imports: [
     CommonModule,
     ToolsRoutingModule,
-    NzInputModule
+    NzInputModule,
+    NzInputNumberModule,
+    NzFormModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ToolsModule { }
